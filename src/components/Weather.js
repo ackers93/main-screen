08464c08fa0f9d876c180cec03a64abdd0c60544
Andrew Ackerman <andrew.ackers@gmail.com>
2020-01-13 20:24:14 -0800

    pushing for Kai

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Weather = props => {
-  console.log(props);
-  return <div className="weather">34°17'13.4"N 118°52'06.4"W</div>;
+const Weather = () => {
+  // initialize state
+  const [moorpark, setMoorpark] = useState("Moorpark");
+  const [data, setData] = useState({
+    isFetching: false,
+    data: null
+  });
+
+  fetch(
+    "https://community-open-weather-map.p.rapidapi.com/weather?lat=34.2856&lon=118.8820&id=2172797&units=%2522imperial%2522&mode=html&q=moorpark%252Cus",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+        "x-rapidapi-key": "f1736cc588mshdf936f14cd9f976p13fb38jsn65705c646b85"
+      }
+    }
+  )
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  return (
+    <div className="weather">
+      <div className="weatherdiv">
+        {(() => {
+          if (data.data)
+            return <div>{/* <p>Weather: {data.weather.temp}</p> */}</div>;
+        })()}
+      </div>
+    </div>
+  );
 };
 
 export default Weather;
-
-// https://www.metaweather.com/api/location/search/?lattlong=>34.28,118.88
